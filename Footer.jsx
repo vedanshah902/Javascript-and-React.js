5e376ad0d82b66a34d1d3dadd3230cde0f92cb59
vedanshah902 <63333891+vedanshah902@users.noreply.github.com>
2021-06-07 13:30:53 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Footer() {
+  var curryear = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright @ {curryear}</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcamp takenby Shaurya Sinha. We covered
+        everything from Scratch including Javascript and React.js,HTML.
+      </p>
+    </div>
+  );
+}
+export default Note;
